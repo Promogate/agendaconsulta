d36c1348df680ded7682c370b31835f31492a00a
Augusto <gutodiasdev@gmail.com>
2024-12-11 08:48:19 -0300

    refact(component): remove SideBar componente from dashboard

--- src/app/dashboard/_components/SideBar.tsx
+++
@@ -1,9 +0,0 @@
-export function SideBar() {
-  return (
-    <aside
-      className={`w-64 bg-white lg:bg-gray-50 border-r border-gray-200 lg:block lg:relative absolute inset-y-0 left-0 z-40 transform transition-transform duration-300 ease-in-out lg:translate-x-0`}
-    >
-      <p>Sidebar</p>
-    </aside>
-  );
-}
